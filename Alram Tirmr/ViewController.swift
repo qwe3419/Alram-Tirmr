1e9770ae3c2a5e76b2a84f0e4764486ec9c984b3
D7703_11 <d7703_11@d7703-11ui-imac.local>
2018-04-18 12:50:58 +0900

    1111ㅇㅁㄴㅇㅁㄴㅇㅁ

--- Alram Tirmr/ViewController.swift
+++ Alram Tirmr/ViewController.swift
@@ -3,39 +3,54 @@
 //  Alram Tirmr
 //
 //  Created by D7703_11 on 2018. 4. 18..
-//  Copyright © 2018년 kimjin. All rights reserved.
+//  Copyright © . All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var myDatepicker: UIDatePicker!
-   @IBOutlet weak var alarmTime: UILabel!
-   @IBOutlet weak var currentTimeLabel: UILabel!
-    var  myTimer = Timer()
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
         
-         let formatter = DateFormatter()
+        // initial Picker Time setting
+        let formatter = DateFormatter()
         formatter.dateFormat = "hh:mm:ss"
-      
+        timeLabel.text = formatter.string(from: myDatePicker.date)
+        
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+            self.updateTime()
+        })
     }
-
-    @IBAction func DatepickerValueChanged(_ sender: Any) {
-       let myAlarmTime = myDatepicker.date
-        print(myAlarmTime)
-     alarmTime.text = String (describing: myAlarmTime)
-     
+    
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        currentTimeLabel.text = formatter.string(from: date)
+        
+        if currentTimeLabel.text == timeLabel.text {
+            view.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func changeDatePicker(_ sender: Any) {
+        print("change Date Picker")
+        print(myDatePicker.date)
+        
+        // dateFormat
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        timeLabel.text = formatter.string(from: myDatePicker.date)
+        
     }
     
     @IBAction func stopAlert(_ sender: Any) {
-        view.backgroundColor = UIColor.blue
+        view.backgroundColor = UIColor.white
     }
 }
-
